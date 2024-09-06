17dfa34546bf5d5cc3396a04f5ad21b0eaf3c395
muteenk <kundangarmuteen@gmail.com>
2024-09-07 00:33:47 +0530

    Updated Folder

--- Frontend/src/components/Heading.jsx
+++
@@ -1,9 +0,0 @@
-export function Heading({ label }) {
-
-    return <>
-        <div className="font-bold text-black text-2xl ">
-            {label}
-        </div>
-
-    </>
-}
--- Frontend/src/components/SubHeading.jsx
+++
@@ -1,9 +0,0 @@
-export function SubHeading({ label }) {
-
-    return <div>
-        <div className="font-bold text-slate-500 text-lg">
-            {label}
-        </div>
-
-    </div >
-}
