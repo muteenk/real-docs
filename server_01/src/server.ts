0d1ffe1e3ad1df1c8676a1a2cc6529d068f05c1c
muteenk <kundangarmuteen@gmail.com>
2024-09-09 01:37:47 +0530

    Upgraded backend to typescript

---
+++ server_01/src/server.ts
@@ -0,0 +1,63 @@
+import app from './app.js';
+import dotenv from 'dotenv';
+dotenv.config({path: './config/config.env'});
+import {Server} from "socket.io";
+import http from "http";
+import cors from "cors";
+
+
+const server = http.createServer(app);
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        credentials: true,
+    }
+});
+
+
+import connectDatabase from '../config/database.js';
+
+
+
+// Config
+const port = process.env.PORT || 5000;
+//connectDatabase();
+
+
+
+// Socket Connection
+io.on("connection", (socket) => {
+    console.log("New Connection -> "+socket.id);
+    socket.emit("welcome", "Welcome to the server");
+    
+    socket.on("disconnect", () => {
+        console.log("User Disconnected -> "+socket.id);
+    })
+
+    socket.on("req", (msg) => {
+        socket.broadcast.emit("res", msg);
+    })
+})
+
+
+
+const serverHandler = server.listen(port, () => {
+    console.log(`server started at http://127.0.0.1:${port}`);
+})
+
+
+// Handle Uncaught Exceptions
+process.on("uncaughtException", err => {
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to Uncaught Exception");
+    process.exit(1);
+});
+
+// Unhandled Promise Rejection
+process.on("unhandledRejection", (err: Error) => {
+    console.log(`Error: ${err.message}`);
+    console.log("Shutting down the server due to Unhandled Promise Rejection");
+    serverHandler.close(() => {
+        process.exit(1);
+    });
+})
