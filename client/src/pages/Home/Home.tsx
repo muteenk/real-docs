b5d96a6d9a227317c33b592334d4a8a46f46f7e3
muteenk <kundangarmuteen@gmail.com>
2024-09-09 04:57:51 +0530

    Improved Textarea Style

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -23,7 +23,7 @@ const Home = ({ws, setWs, docText, setDocText}: HomeProps) => {
           Status: {ws ? <span className="text-[green] font-bold">Online</span> : <span className="text-[red] font-bold">Offline</span>}
       </div>
       <div>
-        <textarea className="block w-[95%] h-[100vh] mx-auto my-[3em] border-2 border-sky-700 outline-none" value={docText} onChange={(e) => {
+        <textarea className="block w-[95%] h-[100vh] mx-auto my-[3em] shadow-[-3px_3px_40px_1px_rgb(0,0,0,0.4)] px-10 py-10 rounded-md outline-none resize-none" value={docText} onChange={(e) => {
           onTyping(e);
         }} ></textarea>
       </div>
