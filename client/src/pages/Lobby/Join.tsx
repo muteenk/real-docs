66d320a45bf496b0d4c7ff677339342362642a79
muteenk <kundangarmuteen@gmail.com>
2024-09-09 23:13:47 +0530

    Join Section in Lobby

--- client/src/pages/Lobby/Join.tsx
+++ client/src/pages/Lobby/Join.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import {useState} from 'react'
 
 const Join = () => {
+
+  const [docId, setDocId] = useState<string>('Document ID will be generated here');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  }
+
   return (
-    <div>Join</div>
+    <>
+      <form className="flex flex-col justify-center items-center h-[60vh]" onSubmit={handleSubmit}>
+        <input type="text" className="border-[1px] border-primary mb-6 w-[90%] md:w-[50%] lg:w-[30%] py-4 px-3 text-center outline-none" placeholder="Enter Document ID" value={docId} onChange={(e) => {setDocId(e.target.value)}} />
+        <button type="submit" className="font-bold px-20 py-2 text-lg shadow-md text-[white] bg-primary" >Join Document Room</button>
+      </form>
+    </>
   )
 }
 
